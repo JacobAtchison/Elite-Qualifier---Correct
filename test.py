04bdc25600a797c7c363e6defc829eac5805dd5d
Millbee <76983560+Millbee@users.noreply.github.com>
2021-02-11 17:32:12 -0600

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 class TestDecideRespons(unittest.TestCase):
 
     def test_build(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
